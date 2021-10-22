1248f1aedab23ddc45f45c7614b526402a0486a8
AlexanderKorobchenko <korobchenko_ua@ukr.net>
2021-10-22 12:22:20 +0300

    add navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { Switch, Route } from 'react-router-dom';
+import Container from './Components/Container';
+import Header from './Components/Header';
+import Home from './Pages/Home';
+import Movies from './Pages/Movies';
+import NotFound from './Pages/NotFound';
+
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <Container>
+      <Header />
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route path="/movies">
+          <Movies />
+        </Route>
+
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </Container>
+  );
 }
 
 export default App;
